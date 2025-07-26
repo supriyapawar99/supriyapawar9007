dc6f075884e8b2bb8712f4336ebdfdb7f09d7af5
supriyapawar99 <pawarsupriya9007@gmail.com>
2025-07-26 12:11:32 +0530

    this is my second program

---
+++ evenodd.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class evenodd{
+public static void main(String args[]){
+for(int i=0;i<100;i++){
+if(i%2!=0){
+	System.out.println(i+"is odd");
+}else{
+	System.out.println(i+"is even");
+}
+}
+}
+}
